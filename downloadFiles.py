91df47812ac0fee019010361315df8e24a37abc0
alspok <alspok@gmail.com>
2023-06-15 18:51:48 +0000

    .

--- downloadFiles.py
+++ downloadFiles.py
@@ -6,7 +6,7 @@ import requests
 from datetime import datetime
 import pytz
 
-@pycron.cron('*/5 * * * *')
+@pycron.cron('*/1 * * * *')
 async def downloadFile(timestamp: datetime) -> None:
     sys.stdout = open("/var/pythonapps/_downloadFiles.out", 'a')
     print(f"Cron job running at {datetime.now(pytz.timezone('Europe/Vilnius')): %Y-%m-%d  %H:%M:%S}", end='   ')
@@ -16,8 +16,7 @@ async def downloadFile(timestamp: datetime) -> None:
         HOSTNAME = "18.195.203.145"
         USERNAME = "yht004"
         PASSWORD = "Hmyhv5pLA70Wbn732T4a"
-        PORT = 22
-        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD, PORT)
+        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
         ftp_server.encoding = "utf-8"
         filename = "pricelist-107377840.csv"
         os.chdir("/var/pythonapps/DataFiles")
@@ -34,8 +33,7 @@ async def downloadFile(timestamp: datetime) -> None:
         HOSTNAME = "194.163.128.173"
         USERNAME = "UABAdemi"
         PASSWORD = "uygvuyi*(YDuiouiy78cyc8T&***YD(*7867987yttTFDDS"
-        PORT = 21
-        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD, PORT)
+        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
         ftp_server.encoding = "utf-8"
         filename = "KARTOTEKI.XML"
         os.chdir("/var/pythonapps/DataFiles")
@@ -48,22 +46,47 @@ async def downloadFile(timestamp: datetime) -> None:
         pass
 
     # Download FTP EETEUROPARTS file eeteuroparts.csv output file Eeteuroparts.csv
+    # try:
+    #     HOSTNAME = "ftp.eetgroup.com"
+    #     USERNAME = "Ledynas"
+    #     PASSWORD = "I1Um77Ignl9Fh172y7DPRjMf"
+    #     PORT = 22
+
+    #     ftps = ftplib.FTP_TLS(HOSTNAME)
+    #     ftps.login(USERNAME, PASSWORD)
+    #     ftps.prot_p()
+
+    #     # ftp_server = ftplib.FTP_TLS(HOSTNAME, USERNAME, PASSWORD, PORT)
+    #     # ftp_server.encoding = "utf-8"
+    #     filename = "/eeteuroparts.csv"
+    #     os.chdir("/var/pythonapps/DataFiles")
+    #     with open(filename, "wb") as file:
+    #         ftps.retrbinary(f"RETR {filename}", file.write)
+    #         # ftp_server.retrbinary(f"RETR {filename}", file.write)
+    #     # os.rename(filename, "Eeteuroparts.csv")
+    #     del filename
+    #     ftp_server.close()
+    # except Exception as e:
+    #     print(e)
+        # pass
+
+    # Donwload FTP APOLO file /MD%20FTP/Apollo%20offer%20CSV.csv
     try:
-        HOSTNAME = "ftp.eetgroup.com"
-        USERNAME = "Ledynas"
-        PASSWORD = "I1Um77Ignl9Fh172y7DPRjMf"
-        PORT = 22
-        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD, PORT)
+        HOSTNAME = "apollowroclawnas.myqnapcloud.com"
+        USERNAME = "zygimantas@ledynas.eu"
+        PASSWORD = "kt9ii427C4PWsyHRNU2G"
+        ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
         ftp_server.encoding = "utf-8"
-        filename = "eeteuroparts.csv"
-        os.chdir("/var/pythonapps/DataFiles")
+        ftp_server.cwd('/MD FTP')
+        filename = "Apollo offer CSV.csv"
         with open(filename, "wb") as file:
             ftp_server.retrbinary(f"RETR {filename}", file.write)
-        os.rename(filename, "Eeteuroparts.csv")
+        os.rename(filename, "Apollo.csv")
         ftp_server.close()
     except Exception as e:
         print(e)
         pass
+
 
     # Download URL GITANA file Gitana.xml outputfile Gitana.xml
     try:
