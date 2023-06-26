d49d7f81799f299c2e9d9e617c2ec443de4539c6
alspok <alspok@gmail.com>
2023-06-26 10:51:53 +0000

    .

--- Classes/ModifyFiles.py
+++ Classes/ModifyFiles.py
@@ -1,39 +1,11 @@
 import csv
 
 class ModifyFiles():
-    def verkkokouppaMod(self) -> None:
-        in_file_name = "/var/pythonapps/DataFiles/Verkkokouppa.csv"
-        out_file_name = "/var/pythonapps/ModDataFiles/Verkkokouppa.mod.csv"
-        with open(f"{in_file_name}", mode='r', encoding='utf-8', errors='ignore') as rfh, \
-             open(f"{out_file_name}", mode='w', encoding='utf-8', newline='') as wfh:
-            for line in rfh:
-                if line.strip():
-                    mod_line = line.replace(',', '.').replace(';', ',')
-                    wfh.write(mod_line)
-
-        with open(f"{out_file_name}", mode='r', encoding='utf=8', errors='ignore') as rfh:
-            csv_dict = csv.DictReader(rfh)
-            dict_list = []
-            for item in csv_dict:
-                dict_list.append(item)
-        
-        eans_list = []
-        for item in dict_list:
-            for key, value in item.items():
-                if key == 'eans':
-                    eans_list.append(value)
-
-        ean_list = []
-        for item in eans_list:
-            ean_list.extend(item.split(':'))
-
-        pass
-
-    
+   
     def verkkokouppaModCSV(self):
         in_file_name = "/var/pythonapps/DataFiles/Verkkokouppa.csv"
-        min_stock = 1
         out_file_name = "/var/pythonapps/ModDataFiles/Verkkokouppa.mod.csv"
+        min_stock = 1
 
         dict_list = []
         with open(in_file_name, mode='r', encoding='utf-8', errors='ignore') as csvfh:
@@ -60,13 +32,13 @@ class ModifyFiles():
         unique_ean_list = []
         unique_item_dict = []
         for item in subst_dict_list:
-            if int(item['ean']) not in unique_ean_list and int(item['stock']) > 0 :
+            if int(item['ean']) not in unique_ean_list and int(item['stock']) >= min_stock :
                 unique_item_dict.append(item)
 
         keys = unique_item_dict[0].keys()
 
         with open(f"{out_file_name}", mode='w', encoding='utf-8', newline='') as mcsvfh:
-            writer = csv.DictWriter(mcsvfh, fieldnames=unique_item_dict[0].keys())
+            writer = csv.DictWriter(mcsvfh, fieldnames=unique_item_dict[0].keys(), delimiter=';')
             writer.writeheader()
             writer.writerows(unique_item_dict)
 
