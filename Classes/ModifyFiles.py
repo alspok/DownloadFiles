0255c445c40434d1978bd6a64987158258454c00
alspok <alspok@gmail.com>
2023-06-27 08:25:39 +0000

    .

--- Classes/ModifyFiles.py
+++ Classes/ModifyFiles.py
@@ -87,6 +87,50 @@ class ModifyFiles():
 
         pass
 
+    def actionMod(self) -> None:
+        in_file_name = "/var/pythonapps/DataFiles/Action.csv"
+        out_file_name = "/var/pythonapps/ModDataFiles/Action.mod.csv"
+        min_stock = 1
+
+        dict_list = []
+        with open(in_file_name, mode='r', encoding='utf-8', errors='ignore') as csvfh:
+            # next(csvfh)
+            csv_reader = csv.DictReader(csvfh, delimiter=',')
+            for row in csv_reader:
+                dict_list.append(row)
+
+        subst_dict_list = []
+        for item in dict_list:
+            subst_dict = {}
+            subst_dict['ean'] = item['EAN']
+            subst_dict['sku'] = item['Manufacturer\'s code']
+            subst_dict['manufacturer'] = item['Producer']
+            subst_dict['title'] = item['Name of product']
+            subst_dict['stock'] = item['Stock']
+            subst_dict['price'] = item['Net price EUR']
+            subst_dict['weight'] = '0'
+
+            subst_dict_list.append(subst_dict)
+
+        unique_ean_list = []
+        unique_item_dict = []
+        for item in subst_dict_list:
+            try:
+                if int(item['ean']) not in unique_ean_list and int(item['stock']) >= min_stock:
+                    unique_item_dict.append(item)
+            except:
+                pass
+
+        fieldnames = unique_item_dict[0].keys()
+
+        with open(f"{out_file_name}", mode='w', encoding='utf-8', newline='') as mcsvfh:
+            writer = csv.DictWriter(mcsvfh, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+            writer.writerows(unique_item_dict)
+        
+
+        pass
+
 
     def gitanaMod() -> None:
 
@@ -100,11 +144,10 @@ class ModifyFiles():
 
         pass
 
-    def actioNod() -> None:
-
-        pass
+   
 
 
 if __name__ == '__main__':
     ModifyFiles().verkkokouppaMod()
     ModifyFiles().apolloMod()
+    ModifyFiles().actionMod()
