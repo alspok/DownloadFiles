44436240ad37f3bc5f54c14d3c2ef45028915f30
alspok <alspok@gmail.com>
2023-06-28 18:42:11 +0000

    .

--- Classes/MenageSQL.py
+++ Classes/MenageSQL.py
@@ -13,7 +13,7 @@ class MenageSQL():
 
         return conn
     
-    def createTable(self) -> None:
+    def createTable(self, conn: object, table_name: str) -> None:
         table_columns = f"(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY," \
                         "company char(255)," \
                         "ean char(255)," \
@@ -24,5 +24,24 @@ class MenageSQL():
                         "price char(255)," \
                         "time_stamps TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"  \
                         "INDEX (ean)) engine=InnoDB"
+        
+        cursor = conn.cursor()
+        query = f"Create table if not exists {table_name} {table_columns}"
+        cursor.execute(query)
+        conn.commit()
+
+        pass
+
+    def dropTable(self, conn, table_name) -> None:
+        query = f"drop table {table_name}"
+        cursor = conn.cursor()
+        cursor. execute(query)
+        conn.commit()
+
+        pass
+
+    def insertTable(self, conn: object, table_name: str) -> None:
+        
+
 
         pass
--- menageSQL.py
+++ menageSQL.py
@@ -1,7 +1,18 @@
-from Classes.MenageSQL import MenageSQL
+from Classes.MenageSQL import MenageSQL as msql
+from Classes.InitValues import InitValues as iv
+import os
 
 def menageSQL() -> None:
-    conn = MenageSQL().connectDB()
+    table_name = 'e_deals_tbl'
+    path = '/var/pythonapps/ModDataFiles'
+
+    conn = msql().connectDB()
+    msql().createTable(conn, table_name)
+
+    file_list = os.listdir(path)
+    for file in file_list:
+        msql().insertTable(conn, table_name)
+    # MenageSQL().dropTable(conn, table_name)
 
     pass
 
