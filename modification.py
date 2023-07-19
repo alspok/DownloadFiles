51c8fa8a5a58aa2dfb39cd92e7f5d407636eddfd
alspok <alspok@gmail.com>
2023-07-19 08:40:46 +0000

    .

---
+++ modification.py
@@ -0,0 +1,18 @@
+from Classes.ModifyFiles import ModifyFiles
+
+def modification() -> None:
+
+    ModifyFiles().verkkokouppaMod() #
+    ModifyFiles().apolloMod() #
+    ModifyFiles().actionMod() #
+    ModifyFiles().domitechMod() #
+    ModifyFiles().gitanaMod() #
+    ModifyFiles().nzdMod() #
+    ModifyFiles().jacobMod() #
+    # ModifyFiles().b2bsportsMod() #
+    # ModifyFiles().eeteuropartsMod()
+
+    pass
+
+if __name__ == '__main__':
+    modification()
