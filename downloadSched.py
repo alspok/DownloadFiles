1c811285acd9a9eafb8815832623f0806c3de8e4
“al <alspok@gmail.com>
2023-08-17 19:03:33 +0000

    .

--- downloadSched.py
+++ downloadSched.py
@@ -9,7 +9,7 @@ def downloadSched() -> None:
     # fdt = dt.strftime("%Y-%m-%d %H:%M:%S")
     # print(f"downloadSched.py runs at: {fdt}")
 
-schedule.every().day.at("00:05").do(downloadSched)
+schedule.every().day.at("05:00").do(downloadSched)
 
 while True:
     schedule.run_pending()
